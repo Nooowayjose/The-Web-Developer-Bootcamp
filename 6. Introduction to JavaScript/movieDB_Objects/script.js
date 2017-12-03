a8ec0a923b241e5ea4acaaf45ea492ff7ec3dc1b
Jose Cabrera <josecabrera244@gmail.com>
2017-12-02 23:16:14 -0600

    Intro to DOM Manipulation Section

--- 6. Introduction to JavaScript/movieDB_Objects/script.js
+++ 6. Introduction to JavaScript/movieDB_Objects/script.js
@@ -1,1 +1,33 @@
 // Movie DB script
+
+
+var movies = [
+  {
+    title: "Wall-e",
+    hasWatched: true,
+    rating: 5
+  },
+  {
+    title: "Pulp-Fiction",
+    hasWatched: true,
+    rating: 5
+  },
+  {
+    title: "Les Miserable",
+    hasWatched: false,
+    rating: 3.5
+  }
+]
+
+
+for (var i = 0; i < movies.length; i++) {
+  var result = "You have ";
+  if (movies[i].hasWatched) {
+    result += "watched ";
+  }else {
+    result += "not seen ";
+  }
+  result += "\"" + movies[i].title + "\" - ";
+  result += movies[i].rating + " stars";
+  console.log(result);
+}
---
+++ 7.DOM Manipulation/Intro to DOM/isBlue.js
@@ -0,0 +1,21 @@
+// script
+
+var h1 = document.querySelector("h1"); //SELECT
+h1.style.color = "pink";
+
+var body = document.querySelector("body"); //SELECT
+var isBlue = false;
+
+// 
+// setInverval(function(){
+//   if(isBlue){
+//     body.style.background = "white";
+//   }else {
+//     body.style.background = "#3498db";
+//   }
+//   isBlue = !isBlue; //Update the boolean expression
+// }, 1000);
+//
+// function setInterval(){
+//
+// }
