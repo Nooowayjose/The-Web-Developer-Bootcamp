36b1e7472f6f37495b7cf33a1a7001a912144844
Jose Cabrera <josecabrera244@gmail.com>
2017-12-09 22:43:51 -0600

    Intro to JQuery

---
+++ 9. JQuery/Code Along 1/script.js
@@ -0,0 +1,18 @@
+// Code Along Script
+
+//When a button w/ the ID trigger is clicked, fadeOut all images
+// $('#trigger').click(function(){
+//   $('body').css("background", "yellow");
+//   $('img').fadeOut(3000, function(){
+//     $(this).remove();
+//   });
+// });
+
+$('#adorable').css("border", "2px solid red");
+
+var styles = {
+  backgroundColor: "pink",
+  fontWeight: "bold"
+};
+
+$('li').css(styles);
---
+++ 9. JQuery/Exercise 1/script.js
@@ -0,0 +1,36 @@
+// script
+// Code Along Script
+
+//When a button w/ the ID trigger is clicked, fadeOut all images
+// $('#trigger').click(function(){
+//   $('body').css("background", "yellow");
+//   $('img').fadeOut(3000, function(){
+//     $(this).remove();
+//   });
+// });
+
+// $('#adorable').css("border", "2px solid red");
+//
+// var styles = {
+//   backgroundColor: "pink",
+//   fontWeight: "bold"
+// };
+//
+// $('li').css(styles);
+
+//First Assignment Code
+
+// var imOrange = {
+//   border: "2px solid orange"
+// };
+
+// Give the divs a pink background color
+// $('div').css("background-color", "purple" );
+// Give the class highlight a width of 200px
+// $('.highlight').css("width", "200px");
+// Give the div with the id third an orange border
+// $('#third').css(imOrange);
+// give the FIRST div, a pink color
+// $('div:first').css("color", "pink");
+//Can also be writen as
+// $('div:eq(0)').css("color", "pink");  //This gives you more control as you can choose which array index
