97b890d66aafd0244815b68236b17fff8ae494e7
Jose Cabrera <josecabrera244@gmail.com>
2017-12-11 19:57:05 -0600

    simple click function

---
+++ 10. Advance jQuery/script.js
@@ -0,0 +1,13 @@
+//Code executed in Chrome console
+//A h1 has been clicked!
+// $('h1').click(function(){
+// 	alert("h1 has been clickeddd!!");
+// });
+
+
+//A button has been clicked
+// $('button').click(function(){
+//
+// 	alert("A button has been clicked!");
+//
+// });
